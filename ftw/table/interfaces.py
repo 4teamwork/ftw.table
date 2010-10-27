5abe8faf9eaba5ba981fac737da9c75f78d726ea
Jonas Baumann <jone@jone.ch>
2010-10-27 16:19:19 +0200

    improved interfaces

--- ftw/table/interfaces.py
+++ ftw/table/interfaces.py
@@ -26,6 +26,32 @@ class ITableSource(Interface):
         Returns the query object.
         """
 
+    def validate_base_query(query):
+        """Validates and fixes the base query. Returns the query object.
+        """
+
+    def extend_query_with_ording(query):
+        """Extends the given `query` with ordering information and returns
+        the new query.
+        """
+
+    def extend_query_with_textfilter(query):
+        """Extends the given `query` with text filters. This is only done when
+        config's `filter_text` is set.
+        """
+
+    def extend_query_with_batching(query):
+        """Extends the given `query` with batching filters and returns the
+        new query. This method is only called when batching is enabled in
+        the source config with the `batching_enabled` attribute.
+        When `lazy` is set to `False` in the configuration, this method is
+        not called.
+        """
+
+    def search_results(query):
+        """Executes the query and returns a tuple of `results` and `length`.
+        """
+
 
 class ITableSourceConfig(Interface):
     """A table source config provides information for the table source such as
@@ -41,7 +67,8 @@ class ITableSourceConfig(Interface):
     order_reverse = schema.Bool(
         title=u'Reverse ordering',
         description=u'If `True` the order of the elements is reversed '
-        'after sorting.')
+        'after sorting.',
+        default=False)
 
     filter_text = schema.TextLine(
         title=u'Filter the elements by text',
@@ -61,9 +88,82 @@ class ITableSourceConfig(Interface):
     batching_current_page = schema.Int(
         title=u'Current page number',
         description=u'The page number the user is currently on, used when '
-        'batching is enabled. The first page is number 1.')
+        'batching is enabled. The first page is number 1.',
+        default=1)
+
+    lazy = schema.Bool(
+        title=u'Make a lazy query',
+        description=u'A lazy query only laods the current page but pretend to '
+        'to have all records. This normally good for performance, but in some '
+        'cases such as "select-all" feature we should respect batching and '
+        'should not be lazy.',
+        default=True)
+
+    def initialize_config():
+        """Is called before get_base_query() is called. This method is used
+        for dynamically configuring / updating the attributes of the config
+        object.
+        """
 
     def get_base_query():
         """Returns the base query for a specific table source type
         (e.g. portal_catalog, sqlalchemy, dict, ...).
         """
+
+
+class ICatalogTableSourceConfig(ITableSourceConfig):
+    """Marker interface for table source configurations used by the
+    portal_catalog table source.
+    See: catalog_source.py
+    """
+
+    filter_path = schema.TextLine(
+        title=u'Path filter',
+        description=u'Only show objects within this path. If the path is None '
+        'no path filter will be applied (all objects are shown). See also '
+        'the `depth` attribute.',
+        default=None)
+
+    depth = schema.Int(
+        title=u'Recursivity depth',
+        description=u'Defines the recursivity depth, how depth the contents '
+        'should be searched. If it set to -1 (default) the recursivity is '
+        'infinite, that means all direct or indirect children are found. If '
+        'set to 1 it will only find direct children. If set to 0 it will only '
+        'find the current context if it matches the other criterias.',
+        depth=-1)
+
+    types = schema.List(
+        title=u'Portal types filter',
+        description=u'Filter results by portal type.',
+        value_type=schema.TextLine(),
+        default=[])
+
+    object_provides = schema.TextLine(
+        title=u'Object provides interface dotted name',
+        description=u'Interface name which the objects have to provide.')
+
+    search_options = schema.Dict(
+        title=u'Additional search options',
+        description=u'Additional options for filtering results.',
+        key_type=schema.TextLine(),
+        value_type=schema.Field(),
+        default={})
+
+    custom_sort_indexes = schema.Dict(
+        title=u'Custom sort indexes',
+        description=u'Provides custom sort mechanisms for certain '
+        'index types.',
+        key_type=schema.TextLine(
+            title=u'portal_catalog index type',
+            description=u'Name of a portal_catalog index type '
+            '(e.g. Products.PluginIndexes.DateIndex.DateIndex)'),
+        value_type=schema.Field(
+            title=u'custom sort method',
+            description=u'pointer to a custom sort method (e.g. '
+            'lambda results, order_by, reverse: ...)'))
+
+    filter_index = schema.TextLine(
+        title=u'Filter index',
+        description=u'Search the `filter_text` in this index.',
+        default='SearchableText')
