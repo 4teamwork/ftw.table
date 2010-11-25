02c0bb67aca5e38371ab1fb9f5c824c5fb811365
Jonas Baumann <jone@jone.ch>
2010-11-25 12:02:19 +0100

    remove whitespace

--- ftw/table/browser/ftwtable.extjs.js
+++ ftw/table/browser/ftwtable.extjs.js
@@ -1,39 +1,39 @@
 //approach for own selection model
 
 // Ext.grid.FTWTableCheckboxSelectionModel = Ext.extend(Ext.grid.RowSelectionModel, {
-// 
-//     
-//     
+//
+//
+//
 //     header : '<div class="x-grid3-hd-checker">&#160;</div>',
-//     
+//
 //     width : 20,
-//     
+//
 //     sortable : false,
-// 
-//     
+//
+//
 //     menuDisabled : true,
 //     fixed : true,
 //     hideable: false,
 //     dataIndex : '',
 //     id : 'checker',
-//     isColumn: true, 
-// 
+//     isColumn: true,
+//
 //     constructor : function(){
 //         Ext.grid.FTWTableCheckboxSelectionModel.superclass.constructor.apply(this, arguments);
 //         if(this.checkOnly){
 //             this.handleMouseDown = Ext.emptyFn;
 //         }
 //     },
-// 
-//     
+//
+//
 //     initEvents : function(){
 //         Ext.grid.FTWTableCheckboxSelectionModel.superclass.initEvents.call(this);
 //         this.grid.on('render', function(){
 //             Ext.fly(this.grid.getView().innerHd).on('mousedown', this.onHdMouseDown, this);
 //         }, this);
 //     },
-// 
-//     
+//
+//
 //     processEvent : function(name, e, grid, rowIndex, colIndex){
 //         console.log(name);
 //         if (name == 'mousedown') {
@@ -43,11 +43,11 @@
 //             return Ext.grid.Column.prototype.processEvent.apply(this, arguments);
 //         }
 //     },
-// 
-//     
+//
+//
 //     onMouseDown : function(e, t){
 //         console.log(123);
-//         if(e.button === 0 && t.className == 'x-grid3-row-checker'){ 
+//         if(e.button === 0 && t.className == 'x-grid3-row-checker'){
 //             e.stopEvent();
 //             var row = e.getTarget('.x-grid3-row');
 //             if(row){
@@ -61,8 +61,8 @@
 //             }
 //         }
 //     },
-// 
-//     
+//
+//
 //     onHdMouseDown : function(e, t) {
 //         if(t.className == 'x-grid3-hd-checker'){
 //             e.stopEvent();
@@ -77,8 +77,8 @@
 //             }
 //         }
 //     },
-// 
-//     
+//
+//
 //     renderer : function(v, p, record){
 //         return '<div class="x-grid3-row-checker">&#160;</div>';
 //     }
@@ -87,32 +87,32 @@
 //
 // create closure
 //
-(function($) {    
-    
+(function($) {
+
     $this = null; // reference to the jQuery table object
     store = null;
     grid = null;
     var options = null;
     var locales = {}; // Stores the translated strings fetched from the server. Use translate(msgid, defaultValue)
-    
-    $.fn.ftwtable.createTable = function(table, url, options){ 
+
+    $.fn.ftwtable.createTable = function(table, url, options){
         options = options;
         $this = table;
         store = new Ext.data.GroupingStore({
             // set up the store
             remoteSort: true,
-            autoLoad: false,  
+            autoLoad: false,
             groupField: '', // kinda ugly way to trick the table into disable grouping by default
             remoteGroup: true,
             autoDestroy:false,
-            
-            //params that will be sent with every request 
-            baseParams: {   
-                ext: 'json', 
+
+            //params that will be sent with every request
+            baseParams: {
+                ext: 'json',
                 tableType: 'extjs', // lets the server know that this is a request from EXTJS ...
                 mode: 'json' // ... and that we want JSON data to be returned
                 },
-            
+
             proxy: new Ext.data.HttpProxy({
                 url: url,
                 method: 'POST',
@@ -121,18 +121,18 @@
                 // autoAbort: true, // Automatically aborts previous AJAX requests
             }),
 
-            // JSON Reader is configured using the data contained in the AJAX response  
+            // JSON Reader is configured using the data contained in the AJAX response
             reader: new Ext.data.JsonReader(),
-            
+
             listeners: {
-                
+
                 // will be called if we get new metadata from the server. E.g. diffrent columns.
                 metachange : function(store, meta){
-                // On metadachange we have to create a new grid. Therefore destroy the old one 
+                // On metadachange we have to create a new grid. Therefore destroy the old one
                 if (grid){
                     grid.destroy();
                 }
-                // translations contains the translated strings that will be used in the ui. 
+                // translations contains the translated strings that will be used in the ui.
                 locales = store.reader.meta.translations;
                 // sorting information
                 store.sortInfo = {
@@ -142,7 +142,7 @@
 
                 var sm =  new Ext.grid.RowSelectionModel();
                 var columns = store.reader.meta.columns;
-                
+
                 // Set up the ColumnModel
                 var cm = new Ext.grid.ColumnModel({
                     columns: columns,
@@ -152,7 +152,7 @@
                             width: 110
                         }
                     });
-                // If we have less than 5 visible columns the grid will be 
+                // If we have less than 5 visible columns the grid will be
                 // rendered with forceFit
                 var visible_columns = 0;
                 var hidden_columns = 0;
@@ -162,12 +162,12 @@
                     if(col.hidden != undefined && col.hidden == true){
                         hidden_columns++;
                     }else{
-                        visible_columns++; 
+                        visible_columns++;
                     }
                 }
                 if(visible_columns<=5){
                     forceFit = true;
-                }    
+                }
                 grid = new Ext.grid.GridPanel({
                     //set up the GridPanel
                     columnLines: true,
@@ -193,7 +193,7 @@
                                    type: "POST",
                                    data: {
                                        new_order: new_order
-                                   } 
+                                   }
                                 });
                             }
                         }
@@ -228,9 +228,9 @@
                                 $('.x-grid3-viewport').css('overflow', 'auto');
                                 //set width of the header div to the same value as the table
                                 var inner_width = $('.x-grid3-header table').width();
-                                $('.x-grid3-header').width(inner_width);   
+                                $('.x-grid3-header').width(inner_width);
                             }
-                            
+
                             /* meta.static contains plain html that we inject into the DOM using key+'_container' as selector.
                             E.G.:
                             "static":{
@@ -238,14 +238,14 @@
                                      [...]
                                   },
                             $('#batching_container.ftwtable') will be replaced with "<!-- Navigation -->"
-                            */ 
+                            */
                             if(store.reader.meta['static'] != undefined){
-                                $.each(store.reader.meta['static'], function(key, value) { 
+                                $.each(store.reader.meta['static'], function(key, value) {
                                     $('#'+key+'_container.ftwtable').html(value);
-                                });   
+                                });
                             }
                             options.onLoad();
-                            
+
                         }
                     }
                 });
@@ -261,7 +261,7 @@
                     grid.render($this.attr('id'));
                 }else{
                     //show message and abord
-                    $('#message_no_contents').show();  
+                    $('#message_no_contents').show();
                     return;
                 }
             }
@@ -272,18 +272,18 @@
         store.load();
 
     };
-    
+
     unlockDragDrop = function(){
       //XXX: We assume that [0] is the GridDragDropRowOrder plugin
-      grid.plugins[0].target.unlock();  
-      grid.ddText = "{0} selected rowen{1}";  
+      grid.plugins[0].target.unlock();
+      grid.ddText = "{0} selected rowen{1}";
       $this.removeClass('draglocked');
     };
-    
+
     lockDragDrop = function(){
       //XXX: We assume that [0] is the GridDragDropRowOrder plugin
-      grid.plugins[0].target.lock();  
-      grid.ddText = translate('dragDropLocked', "Drag 'n' Drop not possible");  
+      grid.plugins[0].target.lock();
+      grid.ddText = translate('dragDropLocked', "Drag 'n' Drop not possible");
       $this.addClass('draglocked');
     };
 
@@ -292,15 +292,15 @@
         if(locales[key]){
             return locales[key];
         }else{
-            return defaultValue || key; 
+            return defaultValue || key;
         }
     };
-    
-    $.fn.ftwtable.reloadTable = function(table, query, options){ 
+
+    $.fn.ftwtable.reloadTable = function(table, query, options){
         $.fn.ftwtable.destroy();
         $.fn.ftwtable.createTable(table, query, options);
     };
-    
+
     $.fn.ftwtable.destroy = function(){
         if(grid){
             grid.destroy();
@@ -312,27 +312,27 @@
         store = null;
         grid = null;
     };
-    
+
     $.fn.ftwtable.select = function(start, end){
         var sm = grid.getSelectionModel();
         if (start=='all'){
             sm.selectRange(0, store.totalLength-1);
         }else if (start && end){
             sm.selectRange(start, end);
-        } else if (end == undefined){ 
+        } else if (end == undefined){
             sm.selectRow(start);
-        }  
+        }
     };
-    
+
     $.fn.ftwtable.deselect = function(start, end){
         var sm = grid.getSelectionModel();
         if (start=='all'){
             sm.deselectRange(0, store.totalLength-1);
         }else if (start && end){
             sm.deselectRange(start, end);
-        } else if (end == undefined){ 
+        } else if (end == undefined){
             sm.deselectRow(start);
-        }  
+        }
     };
 //
 // end of closure
