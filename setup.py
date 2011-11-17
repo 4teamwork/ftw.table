1e7368918a3c23ac07c63abb235f899c80887dbf
Jonas Baumann <jone@jone.ch>
2011-11-17 20:59:05 +0100

    Remove version.txt: declare version in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/table/version.txt').read().strip()
+version = '1.7.2dev'
 maintainer = 'Jonas Baumann'
 
 setup(name='ftw.table',
@@ -9,28 +9,34 @@ setup(name='ftw.table',
       description='Table generator utility for use within zope.',
       long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+
+      keywords='ftw table generator',
       author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
       url='https://github.com/4teamwork/ftw.table',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
         ],
+
       extras_require={
         'extjs': ['collective.js.extjs'],
         },
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
