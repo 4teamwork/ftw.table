cc6ea897d0cc5bce1f98186dc13ca609a53f2103
Jonas Baumann <jone@jone.ch>
2013-01-28 11:22:08 +0100

    Update classifiers.

--- setup.py
+++ setup.py
@@ -17,9 +17,14 @@ setup(name='ftw.table',
 
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
+
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
 
       keywords='ftw table generator',
